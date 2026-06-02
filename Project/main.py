2a9417789792c3e973f179d8458c0ce8bf6d842a
GiagkinisNikolaos <giagkinis.nick@gmail.com>
2026-06-02 20:41:53 +0300

    Revert "test reverse"
    This reverts commit 91e16f0b1d8b4dac681e7468082dc2d67cff8ed3.

--- Project/main.py
+++ Project/main.py
@@ -27,4 +27,4 @@ app = gr.mount_gradio_app(app, gradio_app, path="/ui")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8010)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
